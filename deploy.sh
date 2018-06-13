aedc24227028f7f918071a8d3bcbc8b96af540a5
Werner Buck <email@wernerbuck.nl>
2018-06-13 16:54:09 +0200

    Add secrets for databricks

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export DATABRICKS_TOKEN
+export DATABRICKS_HOST
+
+# do not set token if we already mount in 
+
+if [ -f "~/.databrickscfg" ];
+then 
+  echo "Using supplied configuration"
+else 
+  echo -en "${DATABRICKS_HOST}\n${DATABRICKS_TOKEN}" | databricks configure --token
+done 
+
+# 
